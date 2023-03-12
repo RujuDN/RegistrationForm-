0f7d757b0b0350fabd9fdf30c07fbcd049d39c5e
RujuDN <rujudesai19@gmail.com>
2023-03-12 22:47:16 +0530

    Commit code changes

---
+++ RegistrationFormApp/RegistrationFormWeb/Controllers/RegisterFormController.cs
@@ -0,0 +1,62 @@
+﻿using RegistrationFormWeb.Models;
+using RegistrationFormWeb.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegistrationFormWeb.Controllers
+{
+    public class RegisterFormController : Controller
+    {
+        RegisterDAL registerDAL=new RegisterDAL();
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+        [HttpGet]
+        public ActionResult GetAllDetails()
+        {
+            List<RegistrationFormModel> allDetails = registerDAL.GetAllDetails();
+            return Json(allDetails,JsonRequestBehavior.AllowGet);
+        }
+        
+        [HttpGet]
+        public ActionResult GetAllState()
+        {
+            List<StateModel> stateList = registerDAL.GetAllStates();
+            return Json(stateList, JsonRequestBehavior.AllowGet);
+        }
+       
+        [HttpGet]
+        public ActionResult GetAllCities(int stateId)
+        {
+            List<CityModel> cityList = registerDAL.GetAllCities(stateId);
+            return Json(cityList, JsonRequestBehavior.AllowGet);
+        }
+       
+        [HttpDelete]
+        public ActionResult DeleteDetail(int id)
+        {
+            bool deleted = registerDAL.Deletedetail(id);
+            return Json(deleted, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult GetAllDetailById(int id)
+        {
+            RegistrationFormInsertModel detailById = registerDAL.GetDetailsbyId(id);
+            return Json(detailById, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult InsertUpdateDetails(RegistrationFormInsertModel obj)
+        {
+            bool isInserted = registerDAL.InsertUpdateDetails(obj);
+            return Json(isInserted, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+}
---
+++ RegistrationFormApp/RegistrationFormWeb/Scripts/RegistrationForm.js
@@ -0,0 +1,279 @@
+﻿jQuery(document).ready(function () {
+    GetAllDetails();
+
+    jQuery('#showModal').on('click', function () {
+        jQuery('#registrationFormModal').modal('show');
+    });
+
+    jQuery.ajax({
+        url: "RegisterForm/GetAllState",
+        type: "GET",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+            if (data != null && data.length > 0) {
+                jQuery("#state-dropdown").empty();
+                jQuery.each(data, function (index, row) {
+                    jQuery("#state-dropdown").append("<option value='" + row.Id + "'>" + row.StateName + "</option>")
+                });
+                jQuery("#state-dropdown").get(0).selectedIndex = '-1';
+
+            }
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+
+    jQuery("#state-dropdown").on("change", function () {
+        var stateId = jQuery(this).val();
+        jQuery.ajax({
+            url: "RegisterForm/GetAllCities?stateId=" + stateId,
+            type: "GET",
+            contentType: "application/json;charset=utf-8",
+            dataType: "json",
+            success: function (data) {
+                if (data != null && data.length > 0) {
+                    jQuery("#city-dropdown").empty();
+                    jQuery.each(data, function (index, row) {
+                        jQuery("#city-dropdown").append("<option value='" + row.Id + "'>" + row.CityName + "</option>")
+                    });
+                    jQuery("#city-dropdown").get(0).selectedIndex = '-1';
+                }
+            },
+            error: function (errormessage) {
+                alert(errormessage.responseText);
+            }
+        });
+    });
+
+});
+
+function GetAllDetails() {
+    jQuery.ajax({
+        url: "RegisterForm/GetAllDetails",
+        type: "GET",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            if (result != null && result.length > 0) {
+                var html = '';
+                jQuery.each(result, function (key, item) {
+                    html += '<tr>';
+                    html += '<td>' + item.Id + '</td>';
+                    html += '<td>' + item.Name + '</td>';
+                    html += '<td>' + item.Email + '</td>';
+                    html += '<td>' + item.Phone + '</td>';
+                    html += '<td>' + item.Address + '</td>';
+                    html += '<td>' + item.StateName + '</td>';
+                    html += '<td>' + item.CityName + '</td>';
+                    html += '<td><a href="#" onclick="return getDetailByID(' + item.Id + ')">Edit</a> | <a href="#" onclick="DeleteDetail(' + item.Id + ')">Delete</a></td>';
+                    html += '</tr>';
+                });
+                jQuery('.tbody').html(html);
+            }
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
+
+function InsertUpdateDetail() {
+    var res = validate();
+    if (res == false) {
+        return false;
+    }
+    var regiObj = {
+        Id: jQuery('#hdnID').val(),
+        Name: jQuery('#Name').val(),
+        Age: jQuery('#Age').val(),
+        StateId: jQuery('#state-dropdown').val(),
+        CityId: jQuery('#city-dropdown').val(),
+        Email: jQuery('#Email').val(),
+        Address: jQuery('#Address').val(),
+        Phone: jQuery('#Phone').val(),
+    };
+    if (jQuery('#agree').is(':checked')) {
+        jQuery.ajax({
+            url: "RegisterForm/InsertUpdateDetails",
+            data: JSON.stringify(regiObj),
+            type: "POST",
+            contentType: "application/json;charset=utf-8",
+            dataType: "json",
+            success: function (result) {
+                if (result == true) {
+                    if (regiObj.Id == 0) {
+                        toastr.success('Yes! You have successfully completed your task!', 'Congratulation for you, Mahedee!');
+                    } else {
+                        toastr.success('Yes! You have successfully completed your task!', 'Congratulation for you, Mahedee!');
+                    }
+                }
+                GetAllDetails();
+                jQuery('#registrationFormModal').modal('hide');
+            },
+            error: function (errormessage) {
+                alert(errormessage.responseText);
+            }
+        });
+    }
+    else {
+        alert("Please click on checkbox first");
+    }
+}
+
+function getDetailByID(Id) {
+    jQuery('#Name').css('border-color', 'lightgrey');
+    jQuery('#Age').css('border-color', 'lightgrey');
+    jQuery('#State').css('border-color', 'lightgrey');
+    jQuery('#Country').css('border-color', 'lightgrey');
+    jQuery.ajax({
+        url: "/RegisterForm/GetAllDetailById/" + Id,
+        typr: "GET",
+        contentType: "application/json;charset=UTF-8",
+        dataType: "json",
+        success: function (result) {
+            jQuery('#hdnID').val(result.Id);
+            jQuery('#Name').val(result.Name);
+            jQuery('#Age').val(result.Age);
+            jQuery('#State').val(result.State);
+            jQuery('#Country').val(result.Country);
+
+            jQuery('#registrationFormModal').modal('show');
+            jQuery('#btnUpdate').show();
+            jQuery('#btnSubmit').hide();
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+    return false;
+}
+
+function DeleteDetail(ID) {
+    var ans = confirm("Are you sure you want to delete this Record?");
+    if (ans) {
+        jQuery.ajax({
+            url: "/RegisterForm/DeleteDetail/" + ID,
+            type: "POST",
+            contentType: "application/json;charset=UTF-8",
+            dataType: "json",
+            success: function (result) {
+                GetAllDetails();
+            },
+            error: function (errormessage) {
+                alert(errormessage.responseText);
+            }
+        });
+    }
+}
+
+function clearTextBox() {
+    jQuery('#hdnID').val(0);
+    jQuery('#Name').val("");
+    jQuery('#Age').val("");
+    jQuery('#State').val("");
+    jQuery('#Country').val("");
+    jQuery('#btnUpdate').hide();
+    jQuery('#btnSubmit').show();
+    jQuery('#Name').css('border-color', 'lightgrey');
+    jQuery('#Age').css('border-color', 'lightgrey');
+    jQuery('#State').css('border-color', 'lightgrey');
+    jQuery('#Country').css('border-color', 'lightgrey');
+}
+
+function validate() {
+    var isValid = true;
+    if (jQuery('#Name').val()?.trim() == "") {
+        jQuery('#Name').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        jQuery('#Name').css('border-color', 'lightgrey');
+    }
+    if (jQuery('#Email').val()?.trim() == "") {
+        jQuery('#Email').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        jQuery('#Email').css('border-color', 'lightgrey');
+    }
+
+    if (validateEmail(jQuery('#Email').val()?.trim())) {
+        jQuery('#Email').css({
+            background: "transparent",
+            color: "black"
+        });
+
+    } else {
+        jQuery('#Email').css({
+            color: "red",
+            border: "1px solid red"
+        });
+        return false;
+    }
+
+    if (jQuery('#Age').val()?.trim() == "") {
+        jQuery('#Age').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        jQuery('#Age').css('border-color', 'lightgrey');
+    }
+
+    if (jQuery('#Phone').val()?.trim() == "") {
+        jQuery('#Phone').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        checkPhone(jQuery('#Phone'));
+        jQuery('#Phone').css('border-color', 'lightgrey');
+    }
+
+    if (jQuery('#state-dropdown').val()?.trim() == "") {
+        jQuery('#state-dropdown').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        jQuery('#state-dropdown').css('border-color', 'lightgrey');
+    }
+    if (jQuery('#city-dropdown').val()?.trim() == "") {
+        jQuery('#city-dropdown').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        jQuery('#city-dropdown').css('border-color', 'lightgrey');
+    }
+    return isValid;
+    }
+
+function validateEmail(email) {
+    var pattern = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+
+    return $.trim(email).match(pattern) ? true : false;
+}
+function formatPhone(obj) {
+    var numbers = obj.value.replace(/\D/g, ''),
+        char = { 0: '(', 3: ') ', 6: ' - ' };
+    obj.value = '';
+    for (var i = 0; i < numbers.length; i++) {
+        obj.value += (char[i] || '') + numbers[i];
+    }
+}
+
+function checkPhone(phoneInput) {
+    var formatted = phoneInput.val().replace(/\D/g, '');
+    if (formatted.length !== 10) {
+        jQuery('#Phone').css({
+            color: "red",
+            border: "1px solid red"
+        });
+
+    } else {
+        jQuery('#Phone').css({
+            background: "transparent",
+            color: "black",
+            border:"1px solid #ced4da"
+        });
+    }
+}
